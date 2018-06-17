5f2fc4331ce1717d6577edd62c311ffcbb995fc8
aaronjxu(徐进) <aaronjxu@tencent.com>
2018-06-17 10:58:38 +0800

    add conv—_nn

--- models/conv_nn.py
+++ models/conv_nn.py
@@ -3,4 +3,16 @@ import numpy as np
 
 class ConvNN(object):
 
-    def __init__(self)
+    def __init__(self):
+        pass
+
+    def build_graph(self):
+        pass
+
+    def __model(self):
+        pass
+
+    def __loss(self):
+        pass
+
+        
