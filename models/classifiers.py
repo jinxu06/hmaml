9fd5af5918c1036c69c5c6cc7d8fdef0efe44179
Jin Xu <aaron.jin.xu@gmail.com>
2018-06-28 11:14:43 +0800

    sync

--- models/classifiers.py
+++ models/classifiers.py
@@ -17,9 +17,12 @@ class MNISTClassifier(object):
         self.evals = self._eval(self.outputs, self.targets)
 
     def _model(self, inputs):
+        #kernel_initializer = tf.contrib.layers.xavier_initializer()
+        #kernel_regularizer =
         out = tf.reshape(inputs, (-1, 28, 28, 1))
         for _ in range(2):
-            out = conv2d(out, 16, 3, strides=1, padding='SAME', activation=tf.nn.relu, norm='batch_norm', is_training=self.is_training)
+            out = dense(out, 50, activation=tf.nn.relu, norm="batch_norm", is_training=self.is_training)
+            #out = conv2d(out, 16, 3, strides=1, padding='SAME', activation=tf.nn.relu, norm='batch_norm', is_training=self.is_training)
         out = tf.reshape(out, (-1, int(np.prod(out.get_shape()[1:]))))
         out = dense(out, self.num_classes, activation=None, norm='None', is_training=self.is_training)
         return out
