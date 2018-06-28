d5f7d271f761d86f1bc91712b66c2c74d42f97ca
Jin Xu <aaron.jin.xu@gmail.com>
2018-06-28 11:06:29 +0800

    sync

--- data/mnist.py
+++ data/mnist.py
@@ -7,12 +7,15 @@ def load(batch_size, split=[50000, 10000, 10000], one_hot=True):
     mnist = fetch_mldata('MNIST original', data_home="~/scikit_learn_data")
     images = mnist['data'].astype(np.float32)
     targets = mnist['target'].astype(np.int32)
+    p = np.random.permutation(images.shape[0])
+    images, targets = images[p], targets[p]
     datasets = []
     begin = 0
     for s in split:
         end = begin + s
         X = images[begin:end]
         y = targets[begin:end]
+        print("y:", y.mean())
         X = tf.data.Dataset.from_tensor_slices(X)
         y = tf.data.Dataset.from_tensor_slices(y)
         if one_hot:
