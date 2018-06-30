3590f95139b7909bf9b9e0fc7969ea9e67111401
Jin Xu <aaron.jin.xu@gmail.com>
2018-06-30 17:38:07 +0800

    sync

--- data/mnist.py
+++ data/mnist.py
@@ -4,7 +4,7 @@ from data.dataset import Dataset
 import misc.helpers as helpers
 
 
-def load(data_dir, num_classes, batch_size, split=[5./7, 1./7, 1./7], one_hot=True):
+def load(data_dir, num_classes, batch_size, split=[5./7, 1./7, 1./7], one_hot=True, return_meta=False):
     classes = [5,6,7,8,9] # [0, 1, 2, 3, 4] # np.random.choice(10, num_classes, replace=False).astype(np.int32)
     from sklearn.datasets import fetch_mldata
     mnist = fetch_mldata('MNIST original', data_home=data_dir)
@@ -23,6 +23,19 @@ def load(data_dir, num_classes, batch_size, split=[5./7, 1./7, 1./7], one_hot=Tr
     split[-1] += num_samples - np.sum(split)
     datasets = []
     begin = 0
+    if return_meta:
+        for s in split:
+            end = begin + s
+            X = images[begin:end]
+            X = np.reshape(X, newshape=(s, 28, 28))
+            y = targets[begin:end]
+            y -= np.min(y)
+            if one_hot:
+                y = helpers.one_hot(y, len(classes))
+            dataset = MNIST(inner_batch_size=batch_size, X=X, y=y)
+            datasets.append(dataset)
+            begin = end
+        return datasets 
     for s in split:
         end = begin + s
         X = images[begin:end]
@@ -38,21 +51,24 @@ def load(data_dir, num_classes, batch_size, split=[5./7, 1./7, 1./7], one_hot=Tr
     return datasets
 
 
-class MetaDataset(object):
+class MNIST(object):
 
-    def __init__(self, data_source, inner_batch_size=1, one_hot=True):
-        pass
+    def __init__(self, X, y, inner_batch_size):
+        self.X = X
+        self.y = y
+        self.inner_batch_size = inner_batch_size
 
-    def _load_dataset(self, X, y, num_classes, batch_size, one_hot=True):
-        num_samples = X.shape[0]
-        p = np.random.permutation(X.shape[0])
-        X, y = X[p], y[p]
-        X = tf.data.Dataset.from_tensor_slices(X)
-        y = tf.data.Dataset.from_tensor_slices(y)
-        if one_hot:
-            y = y.map(lambda z: tf.one_hot(z, num_classes))
-        dataset = tf.data.Dataset.zip((X, y)).shuffle(num_samples).batch(batch_size)
-        return dataset
+    # def _load_dataset(self, X, y, num_classes, batch_size, one_hot=True):
+    #     num_samples = X.shape[0]
+    #     p = np.random.permutation(X.shape[0])
+    #     X, y = X[p], y[p]
+    #     X = tf.data.Dataset.from_tensor_slices(X)
+    #     y = tf.data.Dataset.from_tensor_slices(y)
+    #     if one_hot:
+    #         y = y.map(lambda z: tf.one_hot(z, num_classes))
+    #     dataset = tf.data.Dataset.zip((X, y)).shuffle(num_samples).batch(batch_size)
+    #     return dataset
 
-    def sample_mini_dataset(self):
-        pass
+    def sample_mini_dataset(self, num_classes, num_shots, test_shots, classes=None):
+        if classes is None:
+            classes = np.random.choice(10, 5)
---
+++ run_meta_mnist_test.py
@@ -0,0 +1,37 @@
+import numpy as np
+import tensorflow as tf
+from models.classifiers import MNISTClassifier
+from components.learners import Learner
+import data.mnist as mnist
+
+meta_train_set, meta_test_set = mnist.load(data_dir="~/scikit_learn_data", num_classes=5, batch_size=5, split=[5./7, 1./7, 1./7], return_meta=True)
+train_set, test_set = meta_train_set.sample_mini_dataset(num_classes=5, num_shots=15, test_shots=5, classes=[5,6,7,8,9])
+
+
+model = MNISTClassifier(num_classes=5, inputs=None, targets=None)
+update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+with tf.control_dependencies(update_ops):
+    optimizer = tf.train.AdamOptimizer(1e-4).minimize(model.loss)
+
+global_init_op = tf.global_variables_initializer()
+
+saver = tf.train.Saver()
+save_dir = "/data/ziz/jxu/hmaml-saved-models"
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+with tf.Session(config=config) as sess:
+    sess.run(global_init_op)
+
+    # ckpt_file = save_dir + '/params_' + "mnist" + '.ckpt'
+    # print('restoring parameters from', ckpt_file)
+    # saver.restore(sess, ckpt_file)
+
+    learner = Learner(session=sess, model=model)
+    for epoch in range(20):
+        print(epoch, "......")
+        learner.train(train_set, optimizer)
+        evals = learner.evaluate(val_set)
+        print(evals)
+
+    # saver.save(sess, save_dir + '/params_' + "mnist" + '.ckpt')
